adc18b3cd34c2fdfdc8099f8c3547afcf1855f25
ShayminCSAI <shaymincsai@gmail.com>
2021-07-03 23:01:38 +0800

    Add files via upload

---
+++ basic/cmake-build-debug/CMakeFiles/Network.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/shaymin/CLionProjects/Network/echo/echo_client.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/echo/echo_client.c.o"
+  "/Users/shaymin/CLionProjects/Network/echo/echo_server.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/echo/echo_server.c.o"
+  "/Users/shaymin/CLionProjects/Network/handshaking/echo_client2.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/handshaking/echo_client2.c.o"
+  "/Users/shaymin/CLionProjects/Network/handshaking/echo_client_threeway.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/handshaking/echo_client_threeway.c.o"
+  "/Users/shaymin/CLionProjects/Network/handshaking/echo_server2.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/handshaking/echo_server2.c.o"
+  "/Users/shaymin/CLionProjects/Network/handshaking/echo_server_threeway.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/handshaking/echo_server_threeway.c.o"
+  "/Users/shaymin/CLionProjects/Network/hello/hello_client.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/hello/hello_client.c.o"
+  "/Users/shaymin/CLionProjects/Network/hello/hello_server.c" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/hello/hello_server.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/shaymin/CLionProjects/Network/main.cpp" "/Users/shaymin/CLionProjects/Network/cmake-build-debug/CMakeFiles/Network.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
